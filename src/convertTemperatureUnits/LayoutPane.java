74f16df18ef1a8e4775240e6e09264d4191c7be3
Dev-Daljeet <sin10510@sheridancollege.ca>
2020-08-22 10:44:29 -0400

    Fixed few logic errors.
    Signed-off-by: Dev-Daljeet <sin10510@sheridancollege.ca>

--- src/convertTemperatureUnits/LayoutPane.java
+++ src/convertTemperatureUnits/LayoutPane.java
@@ -1,11 +1,9 @@
 package convertTemperatureUnits;
 
-import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
@@ -17,7 +15,6 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
-import javafx.stage.Stage;
 
 public class LayoutPane
 {
