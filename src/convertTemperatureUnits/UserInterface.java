d2a8bffb09a04591b7de6b8c56da69cec735cb11
Dev-Daljeet <sin10510@sheridancollege.ca>
2020-08-22 13:35:07 -0400

    Implemented main() and start() method in class UserInterface.
    Signed-off-by: Dev-Daljeet <sin10510@sheridancollege.ca>

--- src/convertTemperatureUnits/UserInterface.java
+++ src/convertTemperatureUnits/UserInterface.java
@@ -1,4 +1,22 @@
 package convertTemperatureUnits;
 
-public class UserInterface {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class UserInterface extends Application
+{
+    private LayoutPane layout = new LayoutPane();
+    public void start(Stage stage)
+    {
+        stage.setTitle("Temperature Conversion");
+        Scene scene = new Scene(layout.getPaneForLayout(),400,400);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
 }
