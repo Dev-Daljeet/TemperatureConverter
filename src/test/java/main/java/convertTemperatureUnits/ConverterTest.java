361e4e9f1ab1dd9be47b7c82cf8c226f994d9f8b
Dev-Daljeet <sin10510@sheridancollege.ca>
2020-08-23 13:52:55 -0400

    Created and implemented the class ConverterTest for unit testing of class Converter.
    Signed-off-by: Dev-Daljeet <sin10510@sheridancollege.ca>

---
+++ src/test/java/main/java/convertTemperatureUnits/ConverterTest.java
@@ -0,0 +1,110 @@
+package main.java.convertTemperatureUnits;
+
+import main.java.convertTemperatureUnits.Converter;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConverterTest {
+
+    @Test
+    public void onlyInstanceShouldBeCreated() {
+        System.out.println("onlyInstanceShouldBeCreated");
+        Converter onlyInstance = Converter.getInstance();
+        boolean result = onlyInstance instanceof Converter;
+        boolean expResult = true;
+        assertEquals(expResult,result);
+    }
+
+    @Test
+    public void celsiusUnitsShouldBeConverted() {
+        System.out.println("celsiusUnitsShouldbeConverted");
+
+        Converter onlyInstance = Converter.getInstance();
+
+        String resultCel = onlyInstance.celsiusConversion("Celsius (°C)", 18.545);
+        String expResultCel = "18.545 °C = 18.545 °C";
+        assertEquals(expResultCel,resultCel);
+
+        String resultFhr = onlyInstance.celsiusConversion("Fahrenheit (°F)", -33);
+        String expResultFhr = "-33 °C = -27.4 °F";
+        assertEquals(expResultFhr,resultFhr);
+
+        String resultKel = onlyInstance.celsiusConversion("Kelvin (K)", 90);
+        String expResultKel = "90 °C = 363.15 K";
+        assertEquals(expResultKel,resultKel);
+
+        String resultRan = onlyInstance.celsiusConversion("Rankine (°R)", -98.45);
+        String expResultRan = "-98.45 °C = 314.46 °R";
+        assertEquals(expResultRan,resultRan);
+    }
+
+    @Test
+    public void fahrenheitUnitsShouldBeConverted() {
+        System.out.println("fahrenheitUnitsShouldBeConverted");
+
+        Converter onlyInstance = Converter.getInstance();
+
+        String resultCel = onlyInstance.fahrenheitConversion("Celsius (°C)", -44.214);
+        String expResultCel = "-44.214 °F = -42.341 °C";
+        assertEquals(expResultCel,resultCel);
+
+        String resultFhr = onlyInstance.fahrenheitConversion("Fahrenheit (°F)", 40);
+        String expResultFhr = "40 °F = 40 °F";
+        assertEquals(expResultFhr,resultFhr);
+
+        String resultKel = onlyInstance.fahrenheitConversion("Kelvin (K)", 1.34);
+        String expResultKel = "1.34 °F = 256.117 K";
+        assertEquals(expResultKel,resultKel);
+
+        String resultRan = onlyInstance.fahrenheitConversion("Rankine (°R)", -95);
+        String expResultRan = "-95 °F = 364.67 °R";
+        assertEquals(expResultRan,resultRan);
+    }
+
+    @Test
+    public void kelvinUnitsShouldBeConverted() {
+        System.out.println("kelvinUnitsShouldBeConverted");
+
+        Converter onlyInstance = Converter.getInstance();
+
+        String resultCel = onlyInstance.kelvinConversion("Celsius (°C)", 66);
+        String expResultCel = "66 K = -207.15 °C";
+        assertEquals(expResultCel,resultCel);
+
+        String resultFhr = onlyInstance.kelvinConversion("Fahrenheit (°F)", -3.412);
+        String expResultFhr = "-3.412 K = -465.812 °F";
+        assertEquals(expResultFhr,resultFhr);
+
+        String resultKel = onlyInstance.kelvinConversion("Kelvin (K)", -55);
+        String expResultKel = "-55 K = -55 K";
+        assertEquals(expResultKel,resultKel);
+
+        String resultRan = onlyInstance.kelvinConversion("Rankine (°R)", 4.12);
+        String expResultRan = "4.12 K = 7.416 °R";
+        assertEquals(expResultRan,resultRan);
+    }
+
+    @Test
+    public void rankineUnitsShouldBeConverted() {
+        System.out.println("rankineUnitsShouldBeConverted");
+
+        Converter onlyInstance = Converter.getInstance();
+
+        String resultCel = onlyInstance.rankineConversion("Celsius (°C)", -73.22);
+        String expResultCel = "-73.22 °R = -313.828 °C";
+        assertEquals(expResultCel,resultCel);
+
+        String resultFhr = onlyInstance.rankineConversion("Fahrenheit (°F)", 20);
+        String expResultFhr = "20 °R = -439.67 °F";
+        assertEquals(expResultFhr,resultFhr);
+
+        String resultKel = onlyInstance.rankineConversion("Kelvin (K)", 34.567);
+        String expResultKel = "34.567 °R = 19.204 K";
+        assertEquals(expResultKel,resultKel);
+
+        String resultRan = onlyInstance.rankineConversion("Rankine (°R)", -43);
+        String expResultRan = "-43 °R = -43 °R";
+        assertEquals(expResultRan,resultRan);
+    }
+}
